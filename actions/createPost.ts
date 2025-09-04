3a2f1212a068f10549064c6dca89e8ec03f6f9e6
Serdar Geldyev <serdarabad@gmail.com>
2025-09-04 23:23:38 +0300

    revalidatePath for new posts

--- actions/createPost.ts
+++ actions/createPost.ts
@@ -2,6 +2,7 @@
 
 import { uploadImage } from "@/lib/cloudinary";
 import { storePost } from "@/lib/posts";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export default async function createPost(
@@ -44,5 +45,6 @@ export default async function createPost(
     userId: 1,
   });
 
+  revalidatePath("/", "layout");
   redirect("/feed");
 }
